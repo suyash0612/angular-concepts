6d02de8aea3ddc8ac49161cf0f14eb052b6bab26
suyash0612 <suyashsawant1999@gmail.com>
2024-01-19 02:58:56 +0530

    Complete LifeCycle Hooks Tested

--- src/app/lifecycle-hooks/demo/demo.component.ts
+++ src/app/lifecycle-hooks/demo/demo.component.ts
@@ -1,11 +1,14 @@
-import { Component, Input, OnChanges, SimpleChanges, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, OnInit,
+   ViewChild, ElementRef, DoCheck, AfterContentInit,
+    ContentChild, AfterContentChecked, AfterViewInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
-export class DemoComponent implements OnChanges, OnInit {
+export class DemoComponent implements OnChanges, OnInit, 
+DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
   title : string = 'demo component initialised';
   @Input()
   message : string;
@@ -13,20 +16,53 @@ export class DemoComponent implements OnChanges, OnInit {
   @ViewChild('messageRef')
   msgEle : ElementRef;
 
+  @ContentChild('projectedRef')
+  prjCon : ElementRef;
+
   constructor(){
     console.log('Demo Constructor Initialized');
-    console.log(this.title);
-    console.log(this.message);
+    // console.log(this.title);
+    // console.log(this.message);
   }
 
   ngOnChanges(changes : SimpleChanges){
     console.log('ngOnChanges hook called');
     console.log(changes);
-    console.log(this.msgEle.nativeElement);
-
+    // console.log(this.msgEle.nativeElement);
   }
 
   ngOnInit(){
     console.log('ngOnInit hook called');
   }
+
+  ngDoCheck(){
+    console.log('ngDoCheck hook called');
+    // console.log('in ngDoCheck ',this.prjCon);
+  }
+  
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit hook called');
+    // console.log('in ngAfterContentInit ',this.prjCon.nativeElement);
+}
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked hook called');
+    // console.log('in ngAfterContentChecked ',this.prjCon.nativeElement);
+    console.log('in ngAfterContentChecked ',this.msgEle);
+}
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit hook called');
+    console.log('in ngAfterViewInit ',this.msgEle.nativeElement);
+}
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked hook called');
+    console.log('in ngAfterViewChecked ',this.msgEle.nativeElement);
+}
+
+ngOnDestroy() {
+  console.log('ngOnDestroy hook called');
+  }
+
+
 }
--- src/app/lifecycle-hooks/lifecycle-hooks.component.ts
+++ src/app/lifecycle-hooks/lifecycle-hooks.component.ts
@@ -1,16 +1,41 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle-hooks',
   templateUrl: './lifecycle-hooks.component.html',
   styleUrls: ['./lifecycle-hooks.component.css']
 })
-export class LifecycleHooksComponent {
-  inputVal : string;
+export class LifecycleHooksComponent{
+  inputVal : string = '';
+  // inputVal : string = ['Hey, GoodMorning', 'Hello There ! Nice To meet you.'];
   @ViewChild('inputtext') 
   inputRef : ElementRef;
+
+  todestroy : boolean = false;
+
   getTextFromInputField(){
     this.inputVal = this.inputRef.nativeElement.value;
+    // this.inputVal.push(this.inputRef.nativeElement.value);
   }
+
+  
+  constructor(){
+    console.log('life-cycle constructor called')
+  }
+
+  onDestroyBtn(){
+    this.todestroy = !this.todestroy;
+  }
+
+
+//   ngAfterViewInit() {
+//     console.log('lifecycle-hooks Component ngAfterViewInit hook called');
+// }
+
+//   ngAfterViewChecked() {
+//   console.log('lifecycle-hooks Component ngAfterViewChecked hook called');
+//   }
+
+  
 
 }
